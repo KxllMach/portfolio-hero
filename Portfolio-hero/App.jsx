d6dfc713cb7ea37215ed0ca0bd54ce1cfa74e70c
KxllMach <135055683+KxllMach@users.noreply.github.com>
2025-07-18 22:06:58 +0530

    angular damping 0.5

--- Portfolio-hero/App.jsx
+++ Portfolio-hero/App.jsx
@@ -118,7 +118,7 @@ function Connector({ position, children, vec = new THREE.Vector3(), r = THREE.Ma
   return (
     <RigidBody
       linearDamping={2} 
-      angularDamping={0.2} 
+      angularDamping={0.5} 
       friction={0.1}
       restitution={0.9}
       position={pos}
